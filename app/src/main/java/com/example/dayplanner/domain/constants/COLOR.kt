74cdf15ff211962e9887eb558b458e6f05c2c264
Le Duy Anh <lenduya@gmail.com>
2021-01-10 21:26:20 +0100

    Move resource utils to their classes

--- app/src/main/java/com/example/dayplanner/domain/constants/COLOR.kt
+++ app/src/main/java/com/example/dayplanner/domain/constants/COLOR.kt
@@ -1,5 +1,9 @@
 package com.example.dayplanner.domain.constants
 
+import android.content.Context
+import androidx.core.content.ContextCompat
+import com.example.dayplanner.R
+
 enum class COLOR(val rgb: Long) {
     DARK_BLUE(0xff1A237E),
     BURNT_ORANGE(0xffD84315),
@@ -9,5 +13,47 @@ enum class COLOR(val rgb: Long) {
     LIGHT_BLUE(0xff0288D1),
     MAUVE(0xffBA68C8),
     BROWN(0xff795548),
-    TEAL(0xff00897B)
+    TEAL(0xff00897B);
+
+    companion object {
+        fun getId(color: COLOR, context: Context): Int {
+            return when(color) {
+                DARK_BLUE -> {
+                    ContextCompat.getColor(context, R.color.darkBlue)
+                }
+
+                BURNT_ORANGE -> {
+                    ContextCompat.getColor(context, R.color.darkBlue)
+                }
+
+                GREEN -> {
+                    ContextCompat.getColor(context, R.color.green)
+                }
+
+                DARK_RED -> {
+                    ContextCompat.getColor(context, R.color.red)
+                }
+
+                LIME -> {
+                    ContextCompat.getColor(context, R.color.lime)
+                }
+
+                LIGHT_BLUE -> {
+                    ContextCompat.getColor(context, R.color.lightBlue)
+                }
+
+                MAUVE -> {
+                    ContextCompat.getColor(context, R.color.mauve)
+                }
+
+                BROWN -> {
+                    ContextCompat.getColor(context, R.color.brown)
+                }
+
+                TEAL -> {
+                    ContextCompat.getColor(context, R.color.teal)
+                }
+            }
+        }
+    }
 }
--- app/src/main/java/com/example/dayplanner/domain/constants/ICON.kt
+++ app/src/main/java/com/example/dayplanner/domain/constants/ICON.kt
@@ -1,13 +1,25 @@
 package com.example.dayplanner.domain.constants
 
-enum class ICON {
-    FREE_TIME,
-    BREAK,
-    STUDY,
-    WORK,
-    EXERCISE,
-    MENTAL_CULTIVATION,
-    EAT,
-    SLEEP,
-    SHOP
+import android.content.Context
+
+enum class ICON(val iconName: String) {
+    FREE_TIME("ic_free_time"),
+    BREAK("ic_break"),
+    STUDY("ic_study"),
+    WORK("ic_work"),
+    EXERCISE("ic_exercise"),
+    MENTAL_CULTIVATION("ic_bhavana"),
+    EAT("ic_eat"),
+    SLEEP("ic_sleep"),
+    SHOP("ic_shop");
+
+    companion object {
+        fun getId(icon: ICON, context: Context): Int {
+            return context.resources.getIdentifier(
+                icon.iconName,
+                "drawable",
+                context.packageName
+            )
+        }
+    }
 }
