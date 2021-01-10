fa8bd07e543518103248e6c46092580efa67c87c
Le Duy Anh <lenduya@gmail.com>
2021-01-10 18:16:13 +0100

    Create Icons constants

---
+++ app/src/main/java/com/example/dayplanner/domain/constants/ICONS.kt
@@ -0,0 +1,13 @@
+package com.example.dayplanner.domain.constants
+
+enum class ICONS {
+    FREE_TIME,
+    BREAK,
+    STUDY,
+    WORK,
+    EXERCISE,
+    MENTAL_CULTIVATION,
+    EAT,
+    SLEEP,
+    SHOP
+}
